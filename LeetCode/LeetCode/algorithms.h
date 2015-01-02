13d6f112ed0967a3682dc126dbeab0eb062cea38
alanse7en <dxuesong7@gmail.com>
2015-01-02 21:39:41 +0800

    Finish 2 more questions and make more comments

--- LeetCode/LeetCode/algorithms.h
+++ LeetCode/LeetCode/algorithms.h
@@ -12,6 +12,7 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -47,8 +48,9 @@ public:
     }
     /*
      Add two Numbers
+     注意t的连接node的作用，以及不要忘记最后有可能进位
      */
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    static ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* result = NULL;
         ListNode** t = &result;
         int carry = 0; int tmp = 0;
@@ -69,6 +71,10 @@ public:
         return result;
     }
     
+    /*
+     Length of longest sub-string
+     将字符的位置存入map中，通过在map中查询是否出现过当前的字符判断是否重复，注意是否更新lastRepeatPos还取决于当前的LRP和重复字符的位置
+     */
     static int lengthOfLongestSubstring(string s) {
         int result = 0;
         int lastRepeatPos = -1;
@@ -84,9 +90,49 @@ public:
         return result;
     }
     
+    /*
+     Find median of two sorted arrays
+     难点在于对复杂度的要求是log(m+n)，解决寻找第k大的问题之后，中位数的问题自然解决
+     */
+    static double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        if ( (m+n)%2 == 0 )
+            return (findKth(A, m, B, n, (m+n)/2)+
+                    findKth(A, m, B, n, (m+n)/2+1))/2;
+        else
+            return findKth(A, m, B, n, (m+n)/2+1);
+    }
+    
+    /*
+     Count and say
+     比较简单的一题，只需注意将最后的结果读入到字符串中
+     */
+    static string countAndSay(int n) {
+        string seq = "1";
+        int ite = 1;
+        while (ite < n) {
+            stringstream newSeq;
+            char last = seq[0];
+            int count = 0;
+            for (size_t i = 0; i < seq.size(); ++i) {
+                if (last == seq[i])
+                    ++count;
+                else {
+                    newSeq << count << last;
+                    last = seq[i];
+                    count = 1;
+                }
+            }
+            newSeq << count << last;
+            seq = newSeq.str();
+            ++ite;
+        }
+        
+        return seq;
+    }
+    
 private:
     // For addTwoNumbers
-    int getValueAndMoveNext(ListNode* & l) {
+    static int getValueAndMoveNext(ListNode* & l) {
         int x = 0;
         if (l != NULL) {
             x = l->val;
@@ -94,6 +140,25 @@ private:
         }
         
         return x;
+    }
+    // For median of two sorted arrays. Find the k-th element of two sorted arrays
+    // 重点在于舍弃不可能出现第k大的部分，被舍弃的部分的数全都小于第k大的数
+    static double findKth(int A[], int m, int B[], int n, int k) {
+        if (m > n)
+            return findKth(B, n, A, m, k);
+        if (m == 0)
+            return B[k-1];
+        if (k == 1)
+            return min(A[0],B[0]);
+        
+        int pa = min(k/2,m); int pb = k-pa;
+        
+        if (A[pa-1] < B[pb-1])
+            return findKth(A+pa, m-pa, B, n, k-pa);
+        else if (A[pa-1] > B[pb-1])
+            return findKth(A, m, B+pb, n-pb, k-pb);
+        else
+            return A[pa-1];
     }
 };
 
--- LeetCode/LeetCode/test.h
+++ LeetCode/LeetCode/test.h
@@ -36,5 +36,30 @@ TEST(LongestSubStringTest, TestCase) {
     EXPECT_EQ(12, res3);
 }
 
+TEST(MedianOfTwoSortedArraysTest, TestCase) {
+    int A[] = {2,3,4};
+    int B[] = {1};
+    double res = Solution::findMedianSortedArrays(A, 3, B, 1);
+    EXPECT_EQ(2.5, res);
+    
+}
+
+TEST(CountAndSayTest, TestCase) {
+    string s0 = "1";
+    string s1 = "11";
+    string s2 = "21";
+    string s3 = "1211";
+    string s4 = "111221";
+    string s5 = "312211";
+    string s6 = "13112221";
+    
+    EXPECT_EQ(s0, Solution::countAndSay(1));
+    EXPECT_EQ(s1, Solution::countAndSay(2));
+    EXPECT_EQ(s2, Solution::countAndSay(3));
+    EXPECT_EQ(s3, Solution::countAndSay(4));
+    EXPECT_EQ(s4, Solution::countAndSay(5));
+    EXPECT_EQ(s5, Solution::countAndSay(6));
+    EXPECT_EQ(s6, Solution::countAndSay(7));
+}
 
 #endif
